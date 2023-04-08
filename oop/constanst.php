9aee52bca778dd3b7c0c4c9a26e8c266d7bf03b3
Ramadana-hub <danarama154@gmail.com>
2023-04-08 23:54:18 +0800

    OOP

---
+++ oop/constanst.php
@@ -0,0 +1,17 @@
+<?php 
+
+//define('NAMA', 'Effan Najwaini');
+//echo NAMA;
+
+//echo "<br>";
+
+//onst NIP = 1234;
+//echo NIP
+
+class Coba{
+    //define('NAMA', 'Effan Najwaini');
+    const NAMA = 'Muhammad Ramadana';
+}
+
+echo Coba::NAMA;
+?>
